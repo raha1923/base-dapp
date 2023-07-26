ec82224e6f486da7833de0a685490d3a834ad27e
RaHa <mohammadraha313@gmail.com>
2023-07-26 14:46:17 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x75eefa644268925092ca33a0933e56b2c03b57ac";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
